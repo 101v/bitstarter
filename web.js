fe00ff9f600d206000fbaf33facdac84f62b8241
Vimal Patel <mailtovimal@gmail.com>
2013-07-14 22:32:33 +0000

    Solved bug

--- web.js
+++ web.js
@@ -1,22 +1,20 @@
 var express = require('express');
 var fs = require('fs');
 
-var fileContent = function (path) {
+var fileContent = function (path, response) {
   fs.readFile(path, function(err, data) {
     if(err) throw err;
-    return data.toString();
-  }
-}
+    response.send(data.toString());
+  });
+};
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fileContent('./index.html'));
+  fileContent('./index.html', response);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
