2d43e17570be7aada7eeb4b5645d3a489397f117
Vimal Patel <mailtovimal@gmail.com>
2013-07-14 22:06:35 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
+var fs = require('fs');
+
+var fileContent = function (path) {
+  fs.readFile(path, function(err, data) {
+    if(err) throw err;
+    return data.toString();
+  }
+}
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fileContent('./index.html'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
